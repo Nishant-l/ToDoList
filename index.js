55f0f5f7673166e0e38b8a84fddcd2af422c8424
Nishant <nishant3151998@gmail.com>
2020-11-20 15:10:51 +0530

    file strecture setup

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
+// const path=require('path');
 const port=8000;
 const app=express();
 
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const router=express.Router();
-
+const homeControler=require('../controllers/home_controller');
 
 console.log('routs module fetched by index.js to use by app');
+
+router.get('/',homeControler.home);
+
 
 module.exports=router;
